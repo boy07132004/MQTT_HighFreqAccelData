fe8c62b5fbe777357c7123b04f6b3cc005d0e28b
Zheming <boy07132004@gmail.com>
2021-03-26 09:36:48 +0800

    0326

--- ESP32_MQTT_side/src/main.cpp
+++ ESP32_MQTT_side/src/main.cpp
@@ -1,41 +1,105 @@
-#include <Arduino.h>
 #include <WiFi.h>
+#include <sstream>
+#include <string.h>
+#include <Arduino.h>
 #include <AsyncMqttClient.h>
 
-#define MQTT_HOST IPAddress(192, 168, 0, 100)
+#define MQTT_HOST IPAddress(192, 168, 0, 250)
+#define SSID "asus"
+#define PASSWORD "0989212698"
 #define MQTT_PORT 1883
-#define SSID "SSID"
-#define PASSWORD "PASSWORD"
+// #define MQTTUSER "user"
+// #define MQTTPSWD "password"
+
+#define THRESHOLD 30
+
+HardwareSerial sensorSerial(1);
 AsyncMqttClient mqttClient;
+TaskHandle_t task1;
+TaskHandle_t task2;
+
+
+void publishTask(void* pvParameters);
+void subscribeTask(void* pvParameters);
+void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total);
+void publish();
+
 
 void setup(){
-  delay(10);
-  Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin(SSID,PASSWORD);
-  Serial2.begin(500000);
-  while (!WiFi.isConnected()){
-    delay(100);
-  }
+  Serial.begin(115200);
+  Serial.println("Connect to WiFi....");
+  while (!WiFi.isConnected()) delay(100);
+  Serial.println(WiFi.localIP());
+  sensorSerial.begin(250000,SERIAL_8N1,16,17);
   delay(10);
   mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+  mqttClient.setKeepAlive(3);
+  // mqttClient.setCredentials(MQTTUSER,MQTTPSWD);
   mqttClient.connect();
-  
+  Serial.println(WiFi.macAddress());
+  delay(1000);
+  mqttClient.subscribe("ctValue", 0);
+  mqttClient.onMessage(onMqttMessage);
+
+  xTaskCreatePinnedToCore(
+    publishTask,
+    "publishNode",
+    40000,
+    NULL,
+    -1,
+    &task1,
+    tskNO_AFFINITY);
+    
+  xTaskCreatePinnedToCore(
+    subscribeTask,
+    "subscribeNode",
+    40000,
+    NULL,
+    1,
+    &task2,
+    tskNO_AFFINITY);
 }
-#include <string.h>
-bool flag = false;
-String msg ="";
+
+bool POWERON = false;
+bool newData = false;
+String Message;
+
+void publishTask(void* pvParameters){
+  while(true){
+    if(sensorSerial.available()){
+      Message += sensorSerial.readStringUntil('S');
+      newData = true;
+    }
+    if (newData){
+      Message[Message.length()-1] = '\0';
+      if (!mqttClient.connected()) ESP.restart();
+      if (POWERON)
+        mqttClient.publish("vibration",0,true,(char*)Message.c_str());
+      newData = false;
+      Message.clear();
+    }
+
+    delay(1);
+  }
+}
+
+
+void subscribeTask (void* pvParameters){
+  vTaskDelete(NULL);
+}
 
 void loop(){
-  if(Serial2.available()){
-    msg += Serial2.readStringUntil('S');
-    flag = true;
-  }
-  if (flag){
-    msg[msg.length()-1]='\0';
-    if (!mqttClient.connected()) ESP.restart();
-    mqttClient.publish("rand", 2, true,(char*)msg.c_str());
-    flag = false;
-    msg = "";
-  }
+}
+
+
+void onMqttMessage(char *topic, char *payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total)
+{
+  static int ctValue;
+  std::stringstream msg(payload);
+  if (msg >> ctValue);
+  else Serial.println("Error msg");
+  if (ctValue > THRESHOLD) POWERON = true;
+  else POWERON = false;
 }
--- ESP32_sensor_side/src/main.cpp
+++ ESP32_sensor_side/src/main.cpp
@@ -8,6 +8,7 @@
 #define DATA_PER_PKG 25
 #define READING_MAX 65536
 #define SCALE_G 16384.0
+#define FREQ 1000
 
 void Task1code(void* pvParameters);
 void Task2code(void* pvParameters);
@@ -15,14 +16,28 @@ void Task2code(void* pvParameters);
 TaskHandle_t task1;
 TaskHandle_t task2;
 
+
 void setup() {
-  Serial2.begin(500000);
+  Serial2.begin(250000);
   pinMode(SS_PIN, OUTPUT);
 
   SPI.begin();
   SPI.setDataMode(SPI_MODE3);
   SPI.setBitOrder(MSBFIRST);
   SPI.setClockDivider(SPI_CLOCK_DIV8);
+
+   // FIFO Mode
+  digitalWrite(SS_PIN,LOW);
+  SPI.transfer(0x1A);
+  SPI.transfer(0x00);
+  digitalWrite(SS_PIN,HIGH);
+
+  // Bypass DLPF to lift the sample rate limitation from 1khz to 4khz.
+  digitalWrite(SS_PIN,LOW);
+  SPI.transfer(0x1D);
+  SPI.transfer(0x08);
+  digitalWrite(SS_PIN,HIGH);
+
   xTaskCreatePinnedToCore(
     Task1code,
     "Task1",
@@ -42,56 +57,47 @@ void setup() {
     1);
     disableCore0WDT();
 }
-void func(char* *a,char* *b){
-  char* temp = *a;
-  *a = *b;
-  *b = temp;
-}
+
 
 float Trans_value(int in){
   return in < (READING_MAX/2) ? in/SCALE_G : (in-READING_MAX)/SCALE_G;
 }
 
-//char *global=(char*)calloc(30*DATA_PER_PKG+2,sizeof(char));
+
 String global;
-bool flag = false;
+bool newData = false;
 
 void Task1code(void* pvParameters){
   uint8_t buffer[14];
   uint16_t ax,ay,az;
   uint8_t count;
-  //char* msgStr;
   String msgStr;
   unsigned long time_=micros();
-  int interval;
+
   while (true){
-    //msgStr= (char*)calloc(30*DATA_PER_PKG+2,sizeof(char));
     for(count=0;count<DATA_PER_PKG;count++){
-        while (micros()-time_<1000);
-        interval = micros()-time_;
+        while (micros()-time_< (pow10(6)/FREQ);
         time_ = micros();
-        uint8_t i;
+
         digitalWrite(SS_PIN,LOW);
         SPI.transfer(0xbb);
-        for(i=0;i<6;i++){
+        for(uint8_t i=0;i<6;i++){
           buffer[i] = SPI.transfer(0x00);
         }
-
         digitalWrite(SS_PIN, HIGH);
 
         ax = ((buffer[0]) << 8) | buffer[1];
         ay = ((buffer[2]) << 8) | buffer[3];
         az = ((buffer[4]) << 8) | buffer[5];
-        //snprintf(msgStr,30*DATA_PER_PKG+2,"%s%d,%+.4f,%+.4f,%+.4f%s",msgStr,interval,Trans_value(ax),Trans_value(ay),Trans_value(az),count==DATA_PER_PKG-1?"TS":"\n");
-        msgStr = msgStr + String(interval) + ',' + String(Trans_value(ax),4)+ ',' + String(Trans_value(ay),4)+ ',' + String(Trans_value(az),4) + '\n';
+        
+        // msg format : csv -> x,y,z
+        msgStr = msgStr + String(Trans_value(ax),4)+ ',' + String(Trans_value(ay),4)+ ',' + String(Trans_value(az),4) + '\n';
     }
-    //func(&global,&msgStr);
+
     msgStr = msgStr + "S";
     global = msgStr;
     msgStr = "";
-    //free(msgStr);
-    flag = true;
-    //delay(1);
+    newData = true;
   }
   
   };
@@ -99,12 +105,10 @@ void Task1code(void* pvParameters){
 void Task2code(void* pvParameters){
   while(true){
       while (true){
-        if(flag) {
+        if(newData) {
           Serial2.print(global);
-          //free(global);
-          //global = (char*)calloc(30*DATA_PER_PKG+2,sizeof(char));
           global = "";
-          flag = false;
+          newData = false;
           }
         else delay(1);
   }
